7b12531c2f575b69f7a48e2ddd4eacebd6695816
MarcosSo4res <marcosfelipe.soares2014@gmail.com>
2024-11-11 15:52:29 -0300

    Implementação básica do  edit, resolução do problema de encriptação de senha (falta resolver seleção de booleans durante o cadastro [temporáriamente desabilitado ])

--- src/main/java/br/edu/fesa/TotalMedia/service/UserService.java
+++ src/main/java/br/edu/fesa/TotalMedia/service/UserService.java
@@ -6,16 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Service
 public class UserService {
-
+    
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    
     @Autowired
     private UserRepository userRepository;
 
     // Salvar ou atualizar um usuário
     public User saveUser(User user) {
+        // Encriptar a senha antes de salvar
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        return userRepository.save(user);
+    }
+    
+    public User updateUser(User user) {
+        // Verifica se a senha foi fornecida
+        User existingUser = userRepository.findById(user.getId()).orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado"));
+        // Preserve a registrationDate original
+        user.setRegistrationDate(existingUser.getRegistrationDate());
+        
+        if (user.getPassword() != null && !user.getPassword().isEmpty()) {
+            // Encripta a nova senha antes de salvar
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+        } else {
+            // Caso a senha não tenha sido fornecida, mantém a senha atual
+            user.setPassword(existingUser.getPassword()); // Mantém a senha antiga
+        }
+
+        // Atualiza os outros campos (nome, email, etc.)
         return userRepository.save(user);
     }
 
@@ -25,7 +50,7 @@ public class UserService {
     }
 
     // Recuperar um usuário pelo ID
-    public Optional<User> getUserById(UUID id) {
+    public Optional<User> getUserById(int id) {
         return userRepository.findById(id);
     }
 
@@ -35,16 +60,16 @@ public class UserService {
     }
 
     // Deletar um usuário pelo ID
-    public void deleteUser(UUID id) {
+    public void deleteUser(int id) {
         userRepository.deleteById(id);
     }
 
     // Atualizar o status de ativação de um usuário
-    public User toggleUserActiveStatus(UUID id) {
+    public User toggleUserActiveStatus(int id) {
         Optional<User> userOpt = userRepository.findById(id);
         if (userOpt.isPresent()) {
             User user = userOpt.get();
-            user.setActive(!user.isActive());
+            user.setActive(!user.getActive());
             return userRepository.save(user);
         }
         return null;
